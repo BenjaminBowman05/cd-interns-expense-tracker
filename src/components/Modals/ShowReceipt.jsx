4e66c911b8984bd2dcd7e0995d08fd00e4c2f37b
Xavier Cruz <68714224+XavierCruz5106@users.noreply.github.com>
2024-06-06 15:02:53 -0400

    Multi-receipt working

--- src/components/Modals/ShowReceipt.jsx
+++ src/components/Modals/ShowReceipt.jsx
@@ -1,20 +1,96 @@
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
+import { render } from "react-dom";
 
 const ShowReceipt = ({ show, close, data }) => {
-  console.log(data)
+  let idx = 0;
+  let numOfReceipts = data.receipts.length - 1;
+
+  const handleClick = (e) => {
+    console.log("NUM OF REC");
+    console.log(numOfReceipts);
+    let prevBtn = document.getElementById("prev");
+    let nextBtn = document.getElementById("next");
+
+    if (numOfReceipts == 0) {
+      nextBtn.hidden = true;
+      prevBtn.hidden = true;
+      return;
+    }
+
+    if (e.target.id == "next") {
+      if (idx != numOfReceipts) {
+        nextBtn.hidden = false;
+        prevBtn.hidden = false;
+        idx += 1;
+      }
+
+      if (idx == numOfReceipts) {
+        nextBtn.hidden = true;
+      }
+    }
+
+    if (e.target.id == "prev") {
+      if (idx != 0) {
+        prevBtn.hidden = false;
+        nextBtn.hidden = false;
+        idx -= 1;
+      }
+
+      if (idx == 0) {
+        prevBtn.hidden = true;
+      }
+    }
+    console.log("INDEXXX");
+    console.log(idx);
+    document.getElementById("iframe").setAttribute("src", data.receipts[idx]); // update src with the current receipt
+    document.getElementById("receiptsTracker").innerHTML = `${idx + 1}/${
+      numOfReceipts + 1
+    }`;
+  };
+
   return (
-    <Modal size="lg" show={show} onHide={close}>
+    <Modal id="modal" size="lg" show={show} onHide={close}>
       <Modal.Header closeButton>
         <Modal.Title>Receipt</Modal.Title>
       </Modal.Header>
       <Modal.Body id="modal-body" className="d-flex justify-content-center">
-        <embed src={data.receipt} width="500px" height="500px"></embed>
+        <embed
+          id="iframe"
+          src={data.receipts[0]} // inital state of receipt
+          width="500px"
+          height="500px"
+        ></embed>
       </Modal.Body>
-      <Modal.Footer>
-        <Button variant="outline-danger" onClick={close}>
+      <Modal.Footer className="d-flex justify-content-center">
+        {numOfReceipts !== 0 ? (
+          <div>
+            <p id="receiptsTracker">
+              {idx + 1}/{numOfReceipts + 1}
+            </p>
+            <Button
+              id="prev"
+              className="me-2"
+              variant="outline-info"
+              onClick={(e) => handleClick(e)}
+              hidden
+            >
+              {"<"}
+            </Button>
+            <Button
+              id="next"
+              variant="outline-info"
+              onClick={(e) => handleClick(e)}
+            >
+              {">"}
+            </Button>
+          </div>
+        ) : (
+          ""
+        )}
+        {/* <Button variant="outline-danger" onClick={close}>
           Close
-        </Button>
+        </Button> */}
       </Modal.Footer>
     </Modal>
   );
--- src/components/Tables/PurchaseTracker.jsx
+++ src/components/Tables/PurchaseTracker.jsx
@@ -206,7 +206,7 @@ const PurchaseTracker = () => {
                   <td
                     id={`file ${requestInfo.id}`}
                     className={
-                      requestInfo.receipt == ""
+                      requestInfo.receipts[0] == ""
                         ? ""
                         : "d-flex align-items-center"
                     }
@@ -214,7 +214,7 @@ const PurchaseTracker = () => {
                     {requestInfo.ceo &&
                     requestInfo.doo &&
                     requestInfo.requesterSupervisor ? (
-                      requestInfo.receipt == "" ? (
+                      requestInfo.receipts[0] == "" ? (
                         <Form.Control
                           onChange={(e) => handleFileSelect(e, requestInfo.id)}
                           accept=".pdf, .png, .jpeg, .jpg"
@@ -311,7 +311,7 @@ const PurchaseTracker = () => {
         <ShowReceipt
           show={showReceipt}
           close={() => setShowReceipt(false)}
-          data={modalObj}
+          data={modalObj.receipts}
         />
       ) : (
         ""
